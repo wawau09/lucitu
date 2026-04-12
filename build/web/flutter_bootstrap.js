503de2eac6a992e98a40f0b3fad875d7bb67e757
wawau09 <wawatv10@gmail.com>
2026-04-12 18:58:29 +0900

    search

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "810546943" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3662464661" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
